73c2670fda9dd2ee5f980dcd230684e7aa656588
ThunderSphun <manderijn2000@gmail.com>
2022-01-05 08:52:12 +0100

    made code coherent with java style guide

--- src/main/java/gd/rf/acro/scf/ConfigUtils.java
+++ src/main/java/gd/rf/acro/scf/ConfigUtils.java
@@ -11,89 +11,77 @@ import java.util.List;
 import java.util.Map;
 
 public class ConfigUtils {
+	public static Map<String, String> config = new HashMap<>();
+	private static final File file = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/config.acfg");
 
-    public static Map<String,String> config = new HashMap<>();
+	public static Map<String, String> loadConfigs() {
+		try {
+			List<String> lines = FileUtils.readLines(file, "utf-8");
+			lines.forEach(line -> {
+				if (line.charAt(0) != '#') {
+					String noSpace = line.replace(" ", "");
+					String[] entry = noSpace.split("=");
+					config.put(entry[0], entry[1]);
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return config;
+	}
 
+	public static void generateConfigs(List<String> input) {
+		try {
+			FileUtils.writeLines(file, input);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static Map<String,String> loadConfigs()
-    {
-        File file = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/config.acfg");
-        try {
-            List<String> lines = FileUtils.readLines(file,"utf-8");
-            lines.forEach(line->
-            {
-                if(line.charAt(0)!='#')
-                {
-                    String noSpace = line.replace(" ","");
-                    String[] entry = noSpace.split("=");
-                    config.put(entry[0],entry[1]);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return config;
-    }
+	public static Map<String, String> checkConfigs() {
+		if (file.exists()) {
+			return loadConfigs();
+		}
+		generateConfigs(makeDefaults());
+		return loadConfigs();
+	}
 
-    public static void generateConfigs(List<String> input)
-    {
-        File file = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/config.acfg");
+	private static List<String> makeDefaults() {
+		List<String> defaults = new ArrayList<>();
+		defaults.add("#cobblestone funnel ore level (default 5)");
+		defaults.add("cobblelvl=5");
+		defaults.add("#cobblestone funnel speed (default 60)");
+		defaults.add("cobblespeed=60");
 
-        try {
-            FileUtils.writeLines(file,input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		defaults.add("#copper funnel ore level (default 9)");
+		defaults.add("copperlvl=9");
+		defaults.add("#copper funnel speed (default 60)");
+		defaults.add("copperspeed=60");
 
-    public static Map<String,String> checkConfigs()
-    {
-        if(new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/config.acfg").exists())
-        {
-            return loadConfigs();
-        }
-        generateConfigs(makeDefaults());
-        return loadConfigs();
-    }
+		defaults.add("#bronze funnel ore level (default 9)");
+		defaults.add("bronzelvl=9");
+		defaults.add("#bronze funnel speed (default 30)");
+		defaults.add("bronzespeed=30");
 
-    private static List<String> makeDefaults()
-    {
-        List<String> defaults = new ArrayList<>();
-        defaults.add("#cobblestone funnel ore level (default 5)");
-        defaults.add("cobblelvl=5");
-        defaults.add("#cobblestone funnel speed (default 60)");
-        defaults.add("cobblespeed=60");
+		defaults.add("#iron funnel ore level (default 14)");
+		defaults.add("ironlvl=14");
+		defaults.add("#iron funnel speed (default 50)");
+		defaults.add("ironspeed=50");
 
-        defaults.add("#copper funnel ore level (default 9)");
-        defaults.add("copperlvl=9");
-        defaults.add("#copper funnel speed (default 60)");
-        defaults.add("copperspeed=60");
+		defaults.add("#gold funnel ore level (default 17)");
+		defaults.add("goldlvl=17");
+		defaults.add("#gold funnel speed (default 40)");
+		defaults.add("goldspeed=40");
 
-        defaults.add("#bronze funnel ore level (default 9)");
-        defaults.add("bronzelvl=9");
-        defaults.add("#bronze funnel speed (default 30)");
-        defaults.add("bronzespeed=30");
+		defaults.add("#diamond funnel ore level (default -1)");
+		defaults.add("diamondlvl=-1");
+		defaults.add("#diamond funnel speed (default 40)");
+		defaults.add("diamondspeed=40");
 
-        defaults.add("#iron funnel ore level (default 14)");
-        defaults.add("ironlvl=14");
-        defaults.add("#iron funnel speed (default 50)");
-        defaults.add("ironspeed=50");
-
-        defaults.add("#gold funnel ore level (default 17)");
-        defaults.add("goldlvl=17");
-        defaults.add("#gold funnel speed (default 40)");
-        defaults.add("goldspeed=40");
-
-        defaults.add("#diamond funnel ore level (default -1)");
-        defaults.add("diamondlvl=-1");
-        defaults.add("#diamond funnel speed (default 40)");
-        defaults.add("diamondspeed=40");
-
-        defaults.add("#netherite funnel ore level (default -1)");
-        defaults.add("netheritelvl=-1");
-        defaults.add("#netherite funnel speed (default 20)");
-        defaults.add("netheritespeed=20");
-        return defaults;
-    }
-
+		defaults.add("#netherite funnel ore level (default -1)");
+		defaults.add("netheritelvl=-1");
+		defaults.add("#netherite funnel speed (default 20)");
+		defaults.add("netheritespeed=20");
+		return defaults;
+	}
 }
--- src/main/java/gd/rf/acro/scf/SCF.java
+++ src/main/java/gd/rf/acro/scf/SCF.java
@@ -9,7 +9,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.loot.LootTables;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.commons.io.FileUtils;
@@ -17,91 +16,11 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class SCF implements ModInitializer {
-	public static final Identifier SEND_SOUND = new Identifier("scf","send_sound");
-	public static String[] ORES = {};
-	public static String[] WEIGHTS = {};
-	public static final ItemGroup TAB = FabricItemGroupBuilder.build(
-			new Identifier("scf", "tab"),
-			() -> new ItemStack(SCF.COBBLESTONE_FUNNEL_BLOCK));
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		ConfigUtils.checkConfigs();
-		registerBlocks();
-		registerItems();
-		readOreTable();
-		System.out.println("Hello Fabric world!");
-	}
-	public static CobblestoneFunnelBlock COBBLESTONE_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock COPPER_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock BRONZE_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock IRON_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock GOLD_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock DIAMOND_FUNNEL_BLOCK;
-	public static CobblestoneFunnelBlock NETHERITE_FUNNEL_BLOCK;
-	private void registerBlocks()
-	{
-		Map<String,String> c = ConfigUtils.config;
-		COBBLESTONE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("cobblelvl")),Integer.parseInt(c.get("cobblespeed")));
-		COPPER_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("copperlvl")),Integer.parseInt(c.get("copperspeed")));
-		BRONZE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("bronzelvl")),Integer.parseInt(c.get("bronzespeed")));
-		IRON_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("ironlvl")),Integer.parseInt(c.get("ironspeed")));
-		GOLD_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("goldlvl")),Integer.parseInt(c.get("goldspeed")));
-		DIAMOND_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("diamondlvl")),Integer.parseInt(c.get("diamondspeed")));
-		NETHERITE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f,0),Integer.parseInt(c.get("netheritelvl")),Integer.parseInt(c.get("netheritespeed")));
-
-
-
-		Registry.register(Registry.BLOCK,new Identifier("scf","cobblestone_funnel"),COBBLESTONE_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","copper_funnel"),COPPER_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","bronze_funnel"),BRONZE_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","iron_funnel"),IRON_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","gold_funnel"),GOLD_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","diamond_funnel"),DIAMOND_FUNNEL_BLOCK);
-		Registry.register(Registry.BLOCK,new Identifier("scf","netherite_funnel"),NETHERITE_FUNNEL_BLOCK);
-	}
-	private void registerItems()
-	{
-		Registry.register(Registry.ITEM,new Identifier("scf","cobblestone_funnel"),new BlockItem(COBBLESTONE_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","copper_funnel"),new BlockItem(COPPER_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","bronze_funnel"),new BlockItem(BRONZE_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","iron_funnel"),new BlockItem(IRON_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","gold_funnel"),new BlockItem(GOLD_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","diamond_funnel"),new BlockItem(DIAMOND_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-		Registry.register(Registry.ITEM,new Identifier("scf","netherite_funnel"),new BlockItem(NETHERITE_FUNNEL_BLOCK,new Item.Settings().group(TAB)));
-	}
-
-	private void readOreTable()
-	{
-		File file = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/funnel_blocks.acfg");
-		File file2 = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/funnel_blocks_weights.acfg");
-		try
-		{
-			if(!file.exists())
-			{
-				FileUtils.writeLines(file, Arrays.asList(cRES));
-			}
-			ORES= FileUtils.readLines(file,"utf-8").toArray(ORES);
-
-			if(!file2.exists())
-			{
-				FileUtils.writeLines(file2, Arrays.asList(cResRolls));
-			}
-			WEIGHTS= FileUtils.readLines(file2,"utf-8").toArray(WEIGHTS);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static final String[] cRES =
-			{
+	public static final Identifier SEND_SOUND = new Identifier("scf", "send_sound");
+	private static final String[] cRES = {
 					"minecraft:cobblestone",
 					"minecraft:stone",
 					"minecraft:coal_ore",
@@ -129,11 +48,9 @@ public class SCF implements ModInitializer {
 					"c:sapphire_ore",
 					"c:ruby_ore",
 					"minecraft:ancient_debris",
-
 			};
 
-	private static final String[] cResRolls =
-			{
+	private static final String[] cResRolls = {
 					"100",
 					"100",
 					"100",
@@ -161,6 +78,78 @@ public class SCF implements ModInitializer {
 					"20",
 					"20",
 					"1"
-
 			};
+
+	public static String[] ORES = {};
+	public static String[] WEIGHTS = {};
+
+	public static CobblestoneFunnelBlock COBBLESTONE_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock COPPER_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock BRONZE_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock IRON_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock GOLD_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock DIAMOND_FUNNEL_BLOCK;
+	public static CobblestoneFunnelBlock NETHERITE_FUNNEL_BLOCK;
+
+	public static final ItemGroup TAB = FabricItemGroupBuilder.build(
+			new Identifier("scf", "tab"),
+			() -> new ItemStack(SCF.COBBLESTONE_FUNNEL_BLOCK));
+
+	@Override
+	public void onInitialize() {
+		ConfigUtils.checkConfigs();
+
+		registerBlocks();
+		registerItems();
+
+		readOreTable();
+	}
+
+	private void registerBlocks() {
+		Map<String, String> c = ConfigUtils.config;
+
+		COBBLESTONE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("cobblelvl")), Integer.parseInt(c.get("cobblespeed")));
+		COPPER_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("copperlvl")), Integer.parseInt(c.get("copperspeed")));
+		BRONZE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("bronzelvl")), Integer.parseInt(c.get("bronzespeed")));
+		IRON_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("ironlvl")), Integer.parseInt(c.get("ironspeed")));
+		GOLD_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("goldlvl")), Integer.parseInt(c.get("goldspeed")));
+		DIAMOND_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("diamondlvl")), Integer.parseInt(c.get("diamondspeed")));
+		NETHERITE_FUNNEL_BLOCK = new CobblestoneFunnelBlock(AbstractBlock.Settings.of(Material.STONE).ticksRandomly().strength(1.5f, 0), Integer.parseInt(c.get("netheritelvl")), Integer.parseInt(c.get("netheritespeed")));
+
+		Registry.register(Registry.BLOCK, new Identifier("scf", "cobblestone_funnel"), COBBLESTONE_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "copper_funnel"), COPPER_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "bronze_funnel"), BRONZE_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "iron_funnel"), IRON_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "gold_funnel"), GOLD_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "diamond_funnel"), DIAMOND_FUNNEL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scf", "netherite_funnel"), NETHERITE_FUNNEL_BLOCK);
+	}
+
+	private void registerItems() {
+		Registry.register(Registry.ITEM, new Identifier("scf", "cobblestone_funnel"), new BlockItem(COBBLESTONE_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "copper_funnel"), new BlockItem(COPPER_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "bronze_funnel"), new BlockItem(BRONZE_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "iron_funnel"), new BlockItem(IRON_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "gold_funnel"), new BlockItem(GOLD_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "diamond_funnel"), new BlockItem(DIAMOND_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+		Registry.register(Registry.ITEM, new Identifier("scf", "netherite_funnel"), new BlockItem(NETHERITE_FUNNEL_BLOCK, new Item.Settings().group(TAB)));
+	}
+
+	private void readOreTable() {
+		File block = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/funnel_blocks.acfg");
+		File weight = new File(FabricLoader.getInstance().getConfigDirectory().getPath() + "/SCF/funnel_blocks_weights.acfg");
+		try {
+			if (!block.exists()) {
+				FileUtils.writeLines(block, Arrays.asList(cRES));
+			}
+			if (!weight.exists()) {
+				FileUtils.writeLines(weight, Arrays.asList(cResRolls));
+			}
+
+			ORES = FileUtils.readLines(block, "utf-8").toArray(ORES);
+			WEIGHTS = FileUtils.readLines(weight, "utf-8").toArray(WEIGHTS);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
